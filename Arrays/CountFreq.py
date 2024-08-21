968f19795ee7e38268ea620251dfcf4d7c561ebe
Aashish-Rathore <aashishrathore1309@gmail.com>
2024-08-21 22:20:29 +0530

    add

--- Arrays/CountFreq.py
+++ Arrays/CountFreq.py
@@ -1,11 +1,12 @@
 def count_freq(arr):
-    frequency = {}
-    for num in arr:
-        if num in frequency:
-            frequency[num] += 1
-        else:
-            frequency[num]=1
-    return frequency
-
+  freq ={}
+  for num in arr:
+    if num in freq:
+       freq[num]+=1
+    else:
+       freq[num]=1
+  return freq
+      
 arr=[1,2,3,4,5,1,2,3,3,3,3,4,4,4,2,3]
 print(count_freq(arr))
+
