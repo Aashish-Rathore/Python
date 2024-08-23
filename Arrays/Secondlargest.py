d399eda3c5cb2944c6dc43dc83cfab4f3da44ac5
Aashish-Rathore <aashishrathore1309@gmail.com>
2024-08-23 23:22:44 +0530

    add

--- Arrays/Secondlargest.py
+++ Arrays/Secondlargest.py
@@ -1,12 +1,11 @@
 def sec_lar(arr):
-   first = second = float('-inf')
-   for num in arr:
-      if  num > first:
-         second=first
+    first=second=float("-inf")
+    for num in arr:
+        if num > first:
+         second = first 
          first = num
-      elif num > second and num!=first:
-         second=num
-         return second if second!=float('-inf') else None
-     
+        elif num > second and num!=first :
+           second = num
+    return second if second!=float("-inf") else None
 arr=[10,8,5,3,2,1]
 print(sec_lar(arr))
