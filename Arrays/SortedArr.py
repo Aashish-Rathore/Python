f4ddf8dcef8c8af304b6df9a4117adeba0c7e7fd
Aashish-Rathore <aashishrathore1309@gmail.com>
2024-08-24 23:24:51 +0530

    add

--- Arrays/SortedArr.py
+++ Arrays/SortedArr.py
@@ -1,8 +1,8 @@
 def is_sorted(arr):
-    for num in range(len(arr)-1):
+    for num in arr:
         if arr[num]>arr[num+1]:
             return False
         return True
+    return num
 arr=[1,2,3,5,6]
 print(is_sorted(arr))
-
